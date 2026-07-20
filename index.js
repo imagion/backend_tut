a681f5b85eaa02b348c0447f920954c5ecae7dae
Евгений <wfh257@gmail.com>
2026-07-20 19:44:03 +0300

    simple server and simple data

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 // Node's built-in webserver module
 const http = require('http');
 
+let notes = [
+  {
+    id: '1',
+    content: 'HTML is easy',
+    important: true,
+  },
+  {
+    id: '2',
+    content: 'Browser can execute only JavaScript',
+    important: false,
+  },
+  {
+    id: '3',
+    content: 'GET and POST are the most important methods of HTTP protocol',
+    important: true,
+  },
+];
+
 // createServer method of http moduule
 const app = http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/plain' });
-  response.end('Hello World');
+  response.writeHead(200, { 'Content-Type': 'application/json' });
+  response.end(JSON.stringify(notes));
 });
 
 // Listen to server port 3001
