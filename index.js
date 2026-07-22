03ea61d90d270085288f6101a52116571edb976e
Евгений <wfh257@gmail.com>
2026-07-22 23:05:08 +0300

    add middleware

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ const app = express();
 
 app.use(express.json());
 
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method);
+  console.log('Path:  ', request.path);
+  console.log('Body:  ', request.body);
+  console.log('---');
+  next();
+};
+
 let notes = [
   {
     id: '1',
@@ -155,6 +163,12 @@ app.post('/api/persons', (request, response) => {
 
   response.json(person);
 });
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' });
+};
+
+app.use(unknownEndpoint);
 
 const PORT = 3001;
 app.listen(PORT, () => {
