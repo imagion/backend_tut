2ba42741ea860a615f2be2f63b224fab50c20575
Евгений <wfh257@gmail.com>
2026-07-22 15:16:34 +0300

    chal 3-5: get, delete and post requests

--- index.js
+++ index.js
@@ -73,9 +73,27 @@ app.get('/api/notes/:id', (request, response) => {
   }
 });
 
+app.get('/api/persons/:id', (request, response) => {
+  const id = request.params.id;
+  const person = persons.find((person) => person.id === id);
+
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
+
 app.delete('/api/notes/:id', (request, response) => {
   const id = request.params.id;
   notes = notes.filter((note) => note.id !== id);
+
+  response.status(204).end();
+});
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = request.params.id;
+  persons = persons.filter((person) => person.id !== id);
 
   response.status(204).end();
 });
@@ -105,6 +123,30 @@ app.post('/api/notes', (request, response) => {
   notes = notes.concat(note);
 
   response.json(note);
+});
+
+const generateId2 = () => {
+  return persons.length > 0 ? Math.random() * 999999 : 0;
+};
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body;
+
+  if (!body.name | !body.number) {
+    return response.status(400).json({
+      error: 'name or number missing',
+    });
+  }
+
+  const person = {
+    id: generateId2(),
+    name: body.name,
+    number: body.number,
+  };
+
+  persons = persons.concat(person);
+
+  response.json(person);
 });
 
 const PORT = 3001;
