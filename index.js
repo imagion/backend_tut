33bf8a7687f1f62b4169d899806011bacf71cb41
Евгений <wfh257@gmail.com>
2026-07-22 10:59:32 +0300

    chal 2: add info

--- index.js
+++ index.js
@@ -22,27 +22,27 @@ let notes = [
 ];
 
 let persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
+  {
+    id: '1',
+    name: 'Arto Hellas',
+    number: '040-123456',
+  },
+  {
+    id: '2',
+    name: 'Ada Lovelace',
+    number: '39-44-5323523',
+  },
+  {
+    id: '3',
+    name: 'Dan Abramov',
+    number: '12-43-234345',
+  },
+  {
+    id: '4',
+    name: 'Mary Poppendieck',
+    number: '39-23-6423122',
+  },
+];
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>');
@@ -54,6 +54,12 @@ app.get('/api/notes', (request, response) => {
 
 app.get('/api/persons', (request, response) => {
   response.json(persons);
+});
+
+app.get('/info', (request, response) => {
+  response.send(
+    `phonebook has info for ${persons.length} people <br><br> ${Date()}`,
+  );
 });
 
 app.get('/api/notes/:id', (request, response) => {
