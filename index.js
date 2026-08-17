a7398afccf879b56b4c1f9b7637a33345caa088d
Евгений <wfh257@gmail.com>
2026-08-17 21:50:03 +0300

    refactor

--- index.js
+++ index.js
@@ -1,24 +1,10 @@
 require('dotenv').config();
-const Note = require('./models/note');
 const express = require('express');
-const morgan = require('morgan');
-const cors = require('cors');
+const Note = require('./models/note');
 
 const app = express();
-app.use(express.static('dist'));
 
-app.use(express.json());
-
-morgan.token('data', function (req, res) {
-  if (req.method === 'POST') {
-    return JSON.stringify(req.body);
-  }
-});
-app.use(
-  morgan(`:method :url :status :res[content-length] - :response-time ms :data`),
-);
-
-app.use(cors());
+let notes = [];
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method);
@@ -28,23 +14,9 @@ const requestLogger = (request, response, next) => {
   next();
 };
 
-let notes = [
-  {
-    id: '1',
-    content: 'HTML is easy',
-    important: true,
-  },
-  {
-    id: '2',
-    content: 'Browser can execute only JavaScript',
-    important: false,
-  },
-  {
-    id: '3',
-    content: 'GET and POST are the most important methods of HTTP protocol',
-    important: true,
-  },
-];
+app.use(requestLogger);
+app.use(express.static('dist'));
+app.use(express.json());
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>');
@@ -62,30 +34,28 @@ app.get('/api/notes/:id', (request, response) => {
   });
 });
 
+app.post('/api/notes', (request, response) => {
+  const body = request.body;
+
+  if (!body.content) {
+    return response.status(400).json({ error: 'content missing' });
+  }
+
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+  });
+
+  note.save().then((savedNote) => {
+    response.json(savedNote);
+  });
+});
+
 app.delete('/api/notes/:id', (request, response) => {
   const id = request.params.id;
   notes = notes.filter((note) => note.id !== id);
 
   response.status(204).end();
-});
-
-app.post('/api/notes', (request, response) => {
-  const body = request.body;
-
-  if (!body.content) {
-    return response.status(400).json({
-      error: 'content missing',
-    });
-  }
-
-  const note = {
-    content: body.content,
-    important: body.important || false,
-  };
-
-  note.save().then((savedNote) => {
-    response.json(savedNote);
-  });
 });
 
 const unknownEndpoint = (request, response) => {
